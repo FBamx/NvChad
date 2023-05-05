02dfc97c193e8660cce498d4c680ca8cac72e95b
root <root@DESKTOP-0T01DAB>
2023-05-05 23:52:29 +0800

    initialize

---
+++ lua/plugins/configs/treesitter.lua
@@ -0,0 +1,32 @@
+local options = {
+  ensure_installed = {
+    "bash",
+    "comment",
+    "diff",
+    "dockerfile",
+    "dot",
+    "git_rebase",
+    "gitattributes",
+    "gitcommit",
+    "gitignore",
+    "lua",
+    "make",
+    "markdown",
+    "markdown_inline",
+    "mermaid",
+    "python",
+    "regex",
+    "vim",
+    "yaml",
+    "cpp",
+  },
+
+  highlight = {
+    enable = true,
+    use_languagetree = true,
+  },
+
+  indent = { enable = true },
+}
+
+return options
