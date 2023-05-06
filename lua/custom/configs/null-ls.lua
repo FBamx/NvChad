b3f6d3e40460099fafb05d331b63e1dfc7067cde
root <root@DESKTOP-0T01DAB>
2023-05-06 15:32:37 +0800

    add format on save

--- lua/custom/configs/null-ls.lua
+++ lua/custom/configs/null-ls.lua
@@ -9,7 +9,7 @@ local b = null_ls.builtins
 local sources = {
 
   -- webdev stuff
-  b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
+  b.formatting.deno_fmt,                                                    -- choosed deno for ts/js files cuz its very fast!
   b.formatting.prettier.with { filetypes = { "html", "markdown", "css" } }, -- so prettier works only on these filetypes
 
   -- Lua
@@ -28,4 +28,4 @@ null_ls.setup {
 }
 
 -- format on save
-vim.cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.format()]]
+vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format()]]
