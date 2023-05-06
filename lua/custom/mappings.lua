4a5e4b3673915453609c11c06c6d126d49b536dd
root <root@DESKTOP-0T01DAB>
2023-05-06 18:38:41 +0800

    add something

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -10,30 +10,39 @@ M.general = {
 M.tmuxNavigator = {
   plugin = true,
   n = {
-    ["<C-h>"] = {"<cmd> TmuxNavigateLeft<CR>", "tmux h"},
-    ["<C-l>"] = {"<cmd> TmuxNavigateRight<CR>", "tmux l"},
-    ["<C-j>"] = {"<cmd> TmuxNavigateDown<CR>", "tmux j"},
-    ["<C-k>"] = {"<cmd> TmuxNavigateUp<CR>", "tmux k"},
-  }
+    ["<C-h>"] = { "<cmd> TmuxNavigateLeft<CR>", "tmux h" },
+    ["<C-l>"] = { "<cmd> TmuxNavigateRight<CR>", "tmux l" },
+    ["<C-j>"] = { "<cmd> TmuxNavigateDown<CR>", "tmux j" },
+    ["<C-k>"] = { "<cmd> TmuxNavigateUp<CR>", "tmux k" },
+  },
 }
 
 M.SymbolOutline = {
   -- plugin = true,
   n = {
-    ["<leader>tb"] = {"<cmd> SymbolsOutline<CR>", "open SymbolOutline"}
-  }
+    ["<leader>tb"] = { "<cmd> SymbolsOutline<CR>", "open SymbolOutline" },
+  },
 }
 
 M.Lazy = {
   n = {
-    ["<leader>la"] = {"<cmd> Lazy<CR>", "lazy"}
-  }
+    ["<leader>la"] = { "<cmd> Lazy<CR>", "lazy" },
+  },
 }
 
 M.LazyGit = {
   n = {
-    ["<leader>gg"] = {"<cmd> LazyGit<CR>", "LazyGit"}
-  }
+    ["<leader>gg"] = { "<cmd> LazyGit<CR>", "LazyGit" },
+  },
+}
+
+M.Git = {
+  n = {
+    ["<leader>rh"] = { ":lua require 'gitsigns'.reset_hunk()<CR>", "git reset" },
+    ["<leader>ph"] = { ":lua require 'gitsigns'.preview_hunk()<CR>", "git preview" },
+    ["<leader>gb"] = { ":lua package.loaded.gitsigns.blame_line()<CR>", "git blame_line" },
+    ["<leader>td"] = { ":lua require 'gitsigns'.toggle_deleted()<CR>", "git toggle_delete" },
+  },
 }
 
 -- more keybinds!
