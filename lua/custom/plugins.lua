72e87139437e344346001b226b31b961f2d3ddad
root <root@DESKTOP-0T01DAB>
2023-05-15 19:26:06 +0800

    add tabnine plugins

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -320,6 +320,25 @@ local plugins = {
       require("ufo").setup(opts)
     end,
   },
+
+  -- tabnine
+  {
+    "codota/tabnine-nvim",
+    build = "./dl_binaries.sh",
+    lazy = false,
+    config = function()
+      require("tabnine").setup {
+        disable_auto_comment = true,
+        accept_keymap = "<Tab>",
+        dismiss_keymap = "<C-]>",
+        debounce_ms = 800,
+        suggestion_color = { gui = "#808080", cterm = 244 },
+        exclude_filetypes = { "TelescopePrompt" },
+        log_file_path = nil, -- absolute path to Tabnine log file
+      }
+    end,
+  },
+
   { import = "custom.ui" },
   { import = "custom.dap" },
 }
