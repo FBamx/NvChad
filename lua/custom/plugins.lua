0b799a3a04c28fe7a1b8862c2974f791eabf00a5
root <root@DESKTOP-0T01DAB>
2023-05-16 22:19:01 +0800

    add surround plugin

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -338,6 +338,16 @@ local plugins = {
       }
     end,
   },
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup {
+        -- Configuration here, or leave empty to use defaults
+      }
+    end,
+  },
 
   { import = "custom.ui" },
   { import = "custom.dap" },
